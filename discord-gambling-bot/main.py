0cac159cf51aa89bd01513c0d0e72c6d4e926ac7
= <fuxx222.anmeldung@gmail.com>
2024-03-06 17:16:27 +0100

    [📝] Added token variable (dotenv)

--- discord-gambling-bot/main.py
+++ discord-gambling-bot/main.py
@@ -1,7 +1,20 @@
-
-
 # ToDo's
 # - Set up discord bot
 # - Add gambling mechanics to functions
 # - Add gambling commands
 # - Add Aron's special messages
+
+
+# Imports
+
+
+
+
+from dotenv import load_dotenv
+import os
+
+# Token
+load_dotenv()
+token = os.getenv('TOKEN')
+
+# Bot
